9bf16df10930176733cd00103c0f1bee5fbdf419
shelby palumbo <shelbypalumbo94@gmail.com>
2019-12-14 15:46:52 -0500

    Created a function to call a for loop and simplify my password combination if statements.

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # PasswordGenerator
 <h2>Description</h2>
 This repository contains the files for a random password generator.  
-A Bootstrap layout, along with additional external styling was used to style the password generator form.
-Javascript was used for creating interactive buttons to perform the random secure password function.
+A Bootstrap layout, along with additional external styling was used to style the password generator.
+Javascript was used for creating interactive buttons to perform the random password function.
 
 <h2>Usage</h2>
 When the "Generate Secure Password" button is clicked, users will be prompted to choose the length of their randomly generated password; betweem 8 and 128 total characters.
 <img src="images/interface.png" alt="password generator interface">
-After the user submits the length of the password, confirmation boxes will apear that will ask for the specificm characterists the password will from  a choice of:
+After the user submits the length of the password, confirmation boxes will apear that will ask for the specific characterists of the password being generated. <br>
+Users will have a choice of:
   <ul><li>Special Characters</li>
       <li>Numbers</li>
       <li>Uppercase Letters</li>
       <li>Lowercase Letters</li></ul>
 <img src="images/prompt.png" alt="prompt">
-The chosen combinations will decided which portion of the randomPass function will perform and print the generated password to the text box on screen.
-
+The chosen combinations will trigger the if statement with those parameters, which will then use those parameters when calling the generateFun function.
+<img src="images/if_state.png" alt="ifcode"> <br>
+After the generateFun function runs, the generated password  will be returned and printed to the text box on screen.
+<img src="images/result.png" alt="result">
--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+window.onload = function (){
+    var generateButton = document.querySelector("#generate");
+
+    generateButton.addEventListener("click", function(){				
+    document.querySelector("#password").value = randomPass();
+    });
+}
+
+
 function randomPass(){
        
         var length = prompt("How long would you like your password to be? Choose a length between 8 and 128:");
@@ -15,154 +24,102 @@ function randomPass(){
 //TTTT 
             if (useupper === true && uselower === true && usespecial === true && usenum === true){
                 var all = uppercase + lowercase + specialchar + numbers;
-                
-            for (var i = 0; i < length; i++) {
-                var character = Math.floor(Math.random() * all.length);
-                password += all.substring(character, character + 1);
-            }
-            return password;
+                generateFun();
+                return password;
             };
-
 
 //FTTT
             if (useupper === false && uselower === true && usespecial === true && usenum === true) {
                 var all = lowercase + specialchar + numbers;
-    
-            for (var i = 0; i < length; i++) {
-                var character = Math.floor(Math.random() * all.length);
-                password += all.substring(character, character + 1);
-            }
-            return password;
+                generateFun();
+                return password;
             };
-
 
 //FFTT            
             if (useupper === false && uselower === false && usespecial === true && usenum === true) {
                 var all = specialchar + numbers;
-    
-            for (var i = 0; i < length; i++) {
-                var character = Math.floor(Math.random() * all.length);
-                password += all.substring(character, character + 1);
-            }
-            return password;
+                generateFun();
+                return password;
             };
-
 
 //FFFT            
             if (useupper === false && uselower === false && usespecial === false && usenum === true) {
                 var all = numbers;
-    
-            for (var i = 0; i < length; i++) {
-                var character = Math.floor(Math.random() * all.length);
-                password += all.substring(character, character + 1);
-            }
-            return password;
+                generateFun();
+                return password;
             };
-
 
 //TFFF
             if (useupper === true && uselower === false && usespecial === false && usenum === false) {
                 var all = uppercase;
-    
-            for (var i = 0; i < length; i++) {
-                var character = Math.floor(Math.random() * all.length);
-                password += all.substring(character, character + 1);
-            }
-            return password;
+                generateFun();
+                return password;
             };
-
 
 //FTTF
             if  (useupper === false && uselower === true && usespecial === true && usenum === false) {
                 var all = lowercase + specialchar;
-    
-            for (var i = 0; i < length; i++) {
-                var character = Math.floor(Math.random() * all.length);
-                password += all.substring(character, character + 1);
-            }
-            return password;
+                generateFun();
+                return password;
             };
-
 
 //FTFT
             if (useupper === false && uselower === true && usespecial === false && usenum === true) {
                 var all = lowercase + numbers;
-    
-            for (var i = 0; i < length; i++) {
-                var character = Math.floor(Math.random() * all.length);
-                password += all.substring(character, character + 1);
-            }
-            return password;
+                generateFun();
+                return password;
             };
-
 
 
 //TFFT
             if  (useupper === true && uselower === false && usespecial === false && usenum === true) {
                 var all = uppercase + numbers;
-    
-            for (var i = 0; i < length; i++) {
-                var character = Math.floor(Math.random() * all.length);
-                password += all.substring(character, character + 1);
-            }
-            return password;
+                generateFun();
+                return password;
             };
-
 
 //TTFF
             if (useupper === true && uselower === true && usespecial === false && usenum === false) {
                 var all = uppercase + lowercase;
-    
-            for (var i = 0; i < length; i++) {
-                var character = Math.floor(Math.random() * all.length);
-                password += all.substring(character, character + 1);
-            }
-            return password;
+                generateFun();
+                return password;
             };            
-
 
 //TTTF
 
         if (useupper === true && uselower === true && usespecial === true && usenum === false) {
                 var all = uppercase + lowercase + specialchar;
-    
-            for (var i = 0; i < length; i++) {
-                var character = Math.floor(Math.random() * all.length);
-                password += all.substring(character, character + 1);
-            }
-            return password;
+                generateFun();
+                return password;
             };    
             
 //FFTF
          if (useupper === false && uselower === false && usespecial === true && usenum === false) {
                 var all = specialchar;
+                generateFun();
+                return password;
+            };       
+   
+//FTTF
+        if (useupper === false && uselower === true && usespecial === true && usenum === false) {
+            var all = specialchar + lowercase;
+            generateFun();
+            return password;
+        }; 
+        
+        if (useupper === false && uselower === false && usespecial === false && usenum === false) {
+           alert("You must choose at least one password characteristic!");
+            return randomPass();
+        }; 
+     
     
+    
+        function generateFun(){   
             for (var i = 0; i < length; i++) {
                 var character = Math.floor(Math.random() * all.length);
                 password += all.substring(character, character + 1);
-            }
-            return password;
-            };       
-   
-
- //FTTF
-        if (useupper === false && uselower === true && usespecial === true && usenum === false) {
-             var all = specialchar + lowercase;
-
-        for (var i = 0; i < length; i++) {
-            var character = Math.floor(Math.random() * all.length);
-            password += all.substring(character, character + 1);
+            };
         }
-        return password;
-        };            
-     }
-            
-
-
-        window.onload = function (){
-        var generateButton = document.querySelector("#generate");
-
-        generateButton.addEventListener("click", function(){				
-        document.querySelector("#password").value = randomPass();
-        });
+    
     }
+            
